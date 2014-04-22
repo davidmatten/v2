d98606cf3095e2bfb73a4bb5e61447076c7d97cc
dave <dave@dave-Inspiron-N5110.(none)>
2014-04-22 16:10:41 +0200

    added a basic caller. small edit to node.

---
+++ create_node.py
@@ -0,0 +1,8 @@
+from node import node
+print "imported"
+
+n = node()
+print "created a new node"
+
+print "new node random height is: "
+print n.height
--- node.py
+++ node.py
@@ -1,5 +1,10 @@
+import random 
 class node:
     """A simple example node"""
-    name = ""
+    height = None
+    name = None
+
+    def __init__(self):
+        self.height = random.randint(57, 272)
     def get_name(self):
         return self.name
