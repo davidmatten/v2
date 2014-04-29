89a90248e8d300d3499e8d21625055509ef84dd0
dave <dave@dave-Inspiron-N5110.(none)>
2014-04-29 15:12:42 +0200

    added population creator. Nodes get names. added repr function to node.py

---
+++ create_population.py
@@ -0,0 +1,11 @@
+
+from node import node
+
+
+
+# create a population of 100 nodes
+
+population = [node() for x in range(10)]
+
+print population
+
--- node.py
+++ node.py
@@ -1,4 +1,60 @@
 import random 
+
+names = [
+#{{{
+"Homo amans",
+"Homo reciprocans",
+"Homo oeconomicus",
+"Homo ecologicus",
+"Homo combinans",
+"Homo degeneratus",
+"Homo domesticus",
+"Homo faber",
+"Homo ferox",
+"Homo generosus",
+"Homo adorans",
+"Homo ludens",
+"Homo duplex",
+"Homo sociologicus",
+"Homo loquens",
+"Homo loquax",
+"Homo necans",
+"Homo demens",
+"Homo ridens",
+"Homo sentimentalis",
+"Homo politicus",
+"Homo inermis",
+"Homo creator",
+"Homo pictor",
+"Homo aestheticus",
+"Homo grammaticus",
+"Homo imitans",
+"Homo discens",
+"Homo educanus",
+"Homo investigans",
+"Homo excentricus",
+"Homo metaphysicus",
+"Homo religiosus",
+"Homo viator",
+"Homo patiens",
+"Homo laborans",
+"Animal laborans",
+"Animal symbolicum",
+"Animal rationabile",
+"Homo socius",
+"Homo poetica",
+"Homo neophilus and Homo neophobus",
+"Pan narrans",
+"Pan sapiens or Homo troglodytes",
+"Homo mendax",
+"Homo Interneticus",
+"Homo technologicus",
+"Homo sanguinis",
+"Homo mechanicus",
+"Homo peregrinum"
+]
+#}}}
+
 class node:
     """A simple example node"""
     height = None
@@ -6,5 +62,9 @@ class node:
 
     def __init__(self):
         self.height = random.randint(57, 272)
+        self.name = names[random.randint(0, 15)]
+
+    def __repr__(self):
+        return str("Hi, my name is " + self.name + " and I am " + str(self.height) + "cm tall.")
     def get_name(self):
         return self.name
